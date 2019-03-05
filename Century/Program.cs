0130bbb0e1413eb8af867837bd12f89ceb9f155c
Thiago Martins Saraiva <thiagomartinssaraiva@gmail.com>
2019-03-04 21:23:12 -0300

    Add Century

---
+++ Century/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Century
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            CenturyFromYear(year);
+        }
+
+        static void CenturyFromYear(int year)
+        {
+            int century = year % 100 == 0 ? year / 100 : year / 100 + 1;
+            Console.WriteLine($"The century is: {century}");
+        }
+    }
+}
