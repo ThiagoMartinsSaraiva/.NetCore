9b52a0c8f17f00735dc3fd69a0fe9a540c0da372
Thiago Martins Saraiva <thiagomartinssaraiva@gmail.com>
2019-03-04 21:02:05 -0300

    Add Descending Order

---
+++ DescendingOrder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace DescendingOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi! Please, enter a number, preferably with more than one digit.");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            DescendingOrderTransformation(number);
+        }
+        
+        static void DescendingOrderTransformation(int number)
+        {
+            number = int.Parse(string.Concat(number.ToString()
+                .OrderByDescending(num => num)));
+                
+            Console.WriteLine($"This is your number transformed: {number}");
+        }
+    }
+}
